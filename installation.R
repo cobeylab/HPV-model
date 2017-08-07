ffeb2df926b91503f9eaa708eaf9d0f8b306356e
slr72090 <slr@uchicago.edu>
2017-08-07 15:08:07 -0500

    installation file for R

---
+++ installation.R
@@ -0,0 +1,27 @@
+################################################
+## Install packages for the analysis 
+################################################
+#!/usr/bin/Rscript
+
+## Create the personal library if it doesn't exist. Ignore a warning if the directory already exists.
+dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
+
+
+## Install multiple packages
+install.packages(c("plyr",
+                 "dplyr",
+                 "tidyr",
+                 "reshape2",
+                 "stringr",
+                 "parallel",
+                 "ggplot2",
+                 "cowplot",
+                 "RSQLite",
+                 "corrplot",
+                 "viridis",
+                 "gridExtra",
+                 "devtools",
+                 "pomp"),
+                 Sys.getenv("R_LIBS_USER"), 
+                 repos = "http://cran.case.edu"
+                 )
