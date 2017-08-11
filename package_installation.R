7f50ccecfa320b1b5bbe15af74dea14ef16b33aa
slr72090 <slr@uchicago.edu>
2017-08-11 14:21:42 -0500

    alternative package installation

---
+++ package_installation.R
@@ -0,0 +1,29 @@
+
+################################################
+## Install packages for the analysis 
+################################################
+#!/usr/bin/Rscript
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# usage
+packages <- c("plyr",
+              "dplyr",
+              "tidyr",
+              "reshape2",
+              "stringr",
+              "parallel",
+              "ggplot2",
+              "cowplot",
+              "RSQLite",
+              "corrplot",
+              "viridis",
+              "gridExtra",
+              "devtools",
+              "pomp")
+ipak(packages)
